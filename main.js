1b0bf0ad2e3594b607909ba09473aa39a454e09d
Ilian Sapundshiev <ilian@tagspaces.org>
2018-01-10 11:49:09 +0100

    cleaning up

--- main.js
+++ main.js
@@ -5,9 +5,6 @@
 'use strict';
 sendMessageToHost({command: 'loadDefaultTextContent'});
 
-var isCordova;
-var isWin;
-var isWeb;
 var jsonEditor;
 var isViewer = true;
 var filePath;
@@ -24,10 +21,6 @@ $(document).ready(function() {
 
   var extSettings;
   loadExtSettings();
-
-  isCordova = parent.isCordova;
-  isWin = parent.isWin;
-  // isWeb = parent.isWeb;
 
   $('#markdownHelpModal').on('show.bs.modal' , function() {
     $.ajax({
